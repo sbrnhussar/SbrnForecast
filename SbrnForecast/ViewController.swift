403a74a971c950f79a0d641410eb16167c142c83
Mac_mini <mac_mini@mac-mini-mac-mini.local>
2018-08-29 16:31:51 +0300

    Initial Commit

---
+++ SbrnForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SbrnForecast
+//
+//  Created by Mac_mini on 29.08.2018.
+//  Copyright © 2018 Mac_mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
